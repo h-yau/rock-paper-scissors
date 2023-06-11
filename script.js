79c0a219b18dd516de801fc20cd59259cb6dd26a
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2023-06-10 22:41:44 -0700

    Revise game and rounds

--- script.js
+++ script.js
@@ -9,21 +9,6 @@ function getComputerChoice() {
     }
 }
 
-
-
-
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
-    if (playerSelection == computerSelection) {
-        return console.log(`${playerSelection} and ${computerSelection.toLowerCase()}! This is a tie!`);
-    }
-    let result = isPlayerWinner(playerSelection, computerSelection);
-    console.log(result);
-        return false;
-    
-}
-
 // to compare numbers instead of strings. -1 is Scissors
 function isPlayerWinner(player, computer) {
     let temPlayer = 0;
@@ -50,7 +35,58 @@ function isPlayerWinner(player, computer) {
     return temPlayer > temComputer;
 }
 
+function getPlayerChoice() {
+    let input;
+    let notFirstTime = false;
+    do {
+        if (notFirstTime) {
+            alert("Valid answers only!");
+        }
+       input = prompt("Which one are you going to use, rock, paper, or scissors?", "").toLowerCase();
+       console.log(input);
+       notFirstTime = true;
+    } while(!(input == "paper" || input == "rock" || input == "scissors"));
+    return input;
+}
 
-const computerSelection = getComputerChoice();
-playRound("Rock", computerSelection);
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+    if (playerSelection == computerSelection) {
+        return `${playerSelection} and ${computerSelection.toLowerCase()}! This is a tie!`;
+    }
+    let result = isPlayerWinner(playerSelection, computerSelection);
+    if (result) {
+        alert(`You won! ${playerSelection} beats ${computerSelection.toLowerCase()}!`);
+        console.log(`You won! ${playerSelection} beats ${computerSelection.toLowerCase()}!`);
+        return 1;
+    } else {
+        alert(`You lose! ${computerSelection} beats ${playerSelection.toLowerCase()}!`);
+        console.log(`You lose! ${computerSelection} beats ${playerSelection.toLowerCase()}!`);
+        return -1;
+    }
+    
+}
 
+function game(rounds) {
+    alert("Welcome! Let's play 5 rounds of rock paper scissors! Shall we begin?");
+    let roundResult = 0;
+    for (i = 0; i < rounds; i++) {
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+        roundResult += playRound(playerSelection, computerSelection);
+        ///////// need to work on keeping track of the loop
+        if (i != (rounds - 1)) {
+            alert(`${rounds - 1 - i} more rounds to go! Let'keep going!`);
+        }
+    }
+    if (roundResult > 0) {
+        alert("Haha! You lost!");
+        console.log("Haha! You lost!");
+    } else {
+        alert("Man, that was tough! Seems like you won! Congratulations!");
+        console.log("Man, that was tough! Seems like you won! Congratulations!");
+    }
+}
+
+game(5);
